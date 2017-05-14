3cbaba2f1bb3c081ef5befb49af6870c7cf4409a
cuong.pham.1606 <phamphucuong1606@gmail.com>
2017-05-14 20:12:05 +0700

    Back up all Prefab and Project 14 May

---
+++ Assets/Used Assets/Scripts/CharacterController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+	public float initialSpeed = 7.0f;
+
+	void Start(){
+		Cursor.lockState = CursorLockMode.Locked;
+
+	}
+
+	void Update(){
+		float speed;
+		if (Input.GetKey ("left shift") || Input.GetKey ("right shift")) {
+			speed = initialSpeed * 2;
+		} else {
+			speed = initialSpeed;
+		}
+		float translation = Input.GetAxis ("Vertical") * speed;
+		float straffe = Input.GetAxis ("Horizontal") * speed;
+		translation *= Time.deltaTime;
+		straffe *= Time.deltaTime;
+
+		transform.Translate (straffe, 0, translation);
+
+		if (Input.GetKeyDown ("escape")) {
+			Cursor.lockState = CursorLockMode.None;
+		}
+	}
+}
