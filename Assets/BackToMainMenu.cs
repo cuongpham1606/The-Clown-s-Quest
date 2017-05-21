baaf8b3ace7935f1c67519b2048d484be519bac7
cuong.pham.1606 <phamphucuong1606@gmail.com>
2017-05-22 02:42:59 +0700

    1st Push 22/5 start scene done

---
+++ Assets/BackToMainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackToMainMenu : MonoBehaviour {
+	public Button mainMenuButton;
+	// Use this for initialization
+	void Start () {
+		Button btn = mainMenuButton.GetComponent<Button> ();
+		btn.onClick.AddListener (BackToStartScene);
+	}
+	
+	void BackToStartScene(){
+		Application.LoadLevel ("StartScene");
+	}
+		
+}
