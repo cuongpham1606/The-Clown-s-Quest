218c51de20c2858aa1482c2a964f4f56cfe073b6
cuong.pham.1606 <phamphucuong1606@gmail.com>
2017-05-21 15:32:25 +0700

    2st upload 21/05

---
+++ Assets/Used Assets/Scripts/FakeClownController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeClownController : MonoBehaviour {
+
+	public bool canRun = true;
+	public Animator anim;
+	public GameObject endPosition;
+	public float walkSpeed;
+	public float runSpeed;
+	public float destroyAfter;
+
+	private BoxCollider boxCollider; 
+
+	private bool isMoving = false;
+
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+		boxCollider = GetComponent<BoxCollider> ();
+		Vector3 startPos = transform.position;
+	}
+
+	void Update(){
+		if (isMoving && transform.position != endPosition.transform.position) {
+			if (canRun)
+			this.transform.position = Vector3.Lerp (transform.position, endPosition.transform.position, runSpeed * Time.deltaTime);
+			else this.transform.position = Vector3.Lerp (transform.position, endPosition.transform.position, walkSpeed * Time.deltaTime);
+			}
+	}
+
+	void OnTriggerEnter (Collider other){
+
+		if (canRun) {
+			anim.SetTrigger ("isRunning");
+		} else {
+			anim.SetTrigger ("isWalking");
+		}
+		isMoving = true;
+		StartCoroutine (DeleteObject ());
+		boxCollider.enabled = false;
+	}
+
+	private IEnumerator DeleteObject(){
+		yield return new WaitForSeconds (destroyAfter);
+		gameObject.SetActive (false);
+	}
+}
---
+++ Assets/Used Assets/Scripts/RealClownController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.FirstPerson
+{
+	public class RealClownController : MonoBehaviour {
+
+		public GameObject player;
+		public bool isReal = true;
+
+		public Animator anim;
+		private FirstPersonController firstPersonController;
+		private BoxCollider boxCollider;
+
+		// Use this for initialization
+		void Start () {
+			anim = GetComponent<Animator> ();
+			firstPersonController = player.GetComponent<FirstPersonController> ();
+			boxCollider = GetComponent<BoxCollider> ();
+		}
+
+		void OnTriggerEnter (Collider other){
+			anim.SetTrigger ("isTurningAround");
+			if (isReal) {
+				StartCoroutine (Die ());
+			} else
+				StartCoroutine (DollReveal ());
+
+		}
+
+		private IEnumerator Die(){
+			firstPersonController.isDying = true;
+			yield return new WaitForSeconds (1.2f);
+			float rotateAngle = - player.transform.rotation.y;
+			Quaternion target = Quaternion.Euler (0, rotateAngle, 0);
+			this.transform.rotation = Quaternion.Slerp (transform.rotation, target, 0.1f); 
+			anim.SetTrigger ("isScaring");
+			yield return new WaitForSeconds (2f);
+			Application.LoadLevel ("Level1");
+		}
+
+		private IEnumerator DollReveal(){
+			firstPersonController.isDying = true;
+			anim.SetTrigger ("isFallingOff");
+			yield return new WaitForSeconds (2f);
+			boxCollider.enabled = false;
+			firstPersonController.isDying = false;
+		}
+	}
+}
